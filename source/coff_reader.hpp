e4ec3fbb38dd31e5e7d7694df88e9e0243a3c64f
Arthur Brainville <ybalrid@ybalrid.info>
2019-03-24 19:31:34 +0100

    Check ClassID inside the COFF header to distinguish extended COFF files (#27)

--- source/coff_reader.hpp
+++ source/coff_reader.hpp
@@ -10,8 +10,16 @@
 
 union COFF_HEADER
 {
+	constexpr static const uint8_t bigobj_classid[16] = {
+		0xc7, 0xa1, 0xba, 0xd1, 0xee, 0xba, 0xa9, 0x4b,
+		0xaf, 0x20, 0xfa, 0xf6, 0x6a, 0xa4, 0xdc, 0xb8,
+	};
+
+	//This is actually a 16byte UUID
+	static_assert(sizeof(bigobj_classid) == sizeof(CLSID));
+
 	bool is_extended() const {
-		return bigobj.Sig1 == 0x0000 && bigobj.Sig2 == 0xFFFF;
+		return bigobj.Sig1 == 0x0000 && bigobj.Sig2 == 0xFFFF && memcmp(&bigobj.ClassID, bigobj_classid, sizeof(CLSID)) == 0 ;
 	}
 
 	IMAGE_FILE_HEADER obj;
